ff4eadaddee4a7cd3112388799664f10cf2ced10
enes-aydogan <ensaydgn66@gmail.com>
2022-11-03 14:22:44 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import {Provider} from 'react-redux';
+import React from 'react';
+
+import NavigationProvider from './src/navigations/auth-navigator';
+
+import store from './src/store/store';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <NavigationProvider></NavigationProvider>
+    </Provider>
+  );
+};
+
+export default App;
